1e5378f9be0651bd28ecc4e032908cd437b5100c
JeroenVerkerk <jdverkerk@gmail.com>
2018-03-08 16:36:08 +0100

    Finished Workshop

---
+++ src/main/java/com/example/bank/App.java
@@ -0,0 +1,18 @@
+package com.example.bank;
+
+public class App{
+
+	public static void main(String args[]){
+
+		SimpleInterest si = new SimpleInterest();
+		double result = si.calculateSimpleInterest(10000, 5, 7);
+		System.out.println("De interest is: " + result);
+
+	}
+	
+	public static double calculateSimpleInterest(double amount, double years, double rate_of_interst) {
+		double simple_interest;
+		simple_interest = amount * years * rate_of_interst;
+		return simple_interest;
+	}
+} 
